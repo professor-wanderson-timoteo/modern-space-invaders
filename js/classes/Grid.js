ffc41d435f1106fd3e5e9291c9132e0886dbdfd1
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-05-23 13:52:13 -0400

    add update function

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -17,13 +17,26 @@ class Grid {
 
     for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
-        this.invaders.push(new Invader({
-          position: {
-            i: i * 30,
-            j: j * 30
-          }
-        }));
+        this.invaders.push(
+          new Invader({
+            position: {
+              i: i * 30,
+              j: j * 30
+            }
+          })
+        );
       }
+    }
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y = 0;
+
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x * 1.15;
+      this.velocity.y = 30;
     }
   }
 }
