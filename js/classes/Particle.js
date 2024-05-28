fb4013a9e5ce755e6d46ff77b81d9efb5951b99e
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-05-28 15:59:09 -0400

    add update function in Particle class

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -18,4 +18,11 @@ class Particle {
     c.closePath();
     c.restore();
   }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if(this.fades) this.opacity -= 0.01;
+  }
 }
