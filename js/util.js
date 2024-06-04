8638ed1f6a54b20dbe7dd5fe8655f101bbac09dd
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-06-04 17:08:18 -0400

    add createParticles function in util.js file.

--- js/util.js
+++ js/util.js
@@ -25,9 +25,29 @@ function createScoreLabel({ score = 100, object }) {
 }
 
 function rectangularCollission({ rectangle1, rectangle2 }) {
-  return(
+  return (
     rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
     rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
     rectangle1.position.x <= rectangle2.position.x + rectangle2.width
   );
+}
+
+function createParticles({ object, color, fades }) {
+  for (let i = 0; i < 15; i++) {
+    particles.push(
+      new Particle({
+        position: {
+          x: object.position.x + object.width / 2,
+          y: object.position.y + object.height / 2
+        },
+        velocity: {
+          x: (Math.random() - 0.5) * 2,
+          y: (Math.random() - 0.5) * 2
+        },
+        radius: Math.random() * 3,
+        color: color || "#BAA0DE",
+        fades: fades
+      })
+    );
+  }
 }
