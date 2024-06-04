cc141dcaa60db6a43ad6aff63b5eed065a5e8027
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-06-04 16:40:15 -0400

    add rectangularCollission function in util.js file.

--- js/util.js
+++ js/util.js
@@ -23,3 +23,11 @@ function createScoreLabel({ score = 100, object }) {
     }
   });
 }
+
+function rectangularCollission({ rectangle1, rectangle2 }) {
+  return(
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+  );
+}
