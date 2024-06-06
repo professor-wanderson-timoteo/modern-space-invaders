d23cfb1678711b9e9d660f0c37accfe25562a93d
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-06-06 17:06:34 -0400

    add validation to update and generate player particles in the animate function

--- index.js
+++ index.js
@@ -166,5 +166,27 @@ function animate() {
       bombs.splice(i, 1);
     } else bomb.update();
   }
+
+  player.update();
+  for(let i = player.particles.length - 1; i >= 0; i--) {
+    const particle = player.particles[i];
+    particle.update();
+    if (particle.opacity === 0) player.particles[i].splice(i, 1);
+  }
+
+  particles.forEach((particle, i) => {
+    if(particle.position.y - particle.radius >= canvas.height) {
+      particle.position.x = Math.random() * canvas.width;
+      particle.position.y = -particle.radius;
+    }
+
+    if(particle.opacity <= 0) {
+      setTimeout(() => {
+        particles.splice(i, 1);
+      }, 0)
+    } else {
+      particle.update();
+    }
+  });
 }
 
