8efcdcf9680a67a058830c401175733bf6ce1649
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-06-06 17:26:24 -0400

    add updates and removes invader projectiles that leave the screen and checks for collisions between projectiles and the player

--- index.js
+++ index.js
@@ -188,5 +188,28 @@ function animate() {
       particle.update();
     }
   });
+
+  invaderProjectiles.forEach((invaderProjectile, index) => {
+    if (
+      invaderProjectile.position.y + invaderProjectile.height >=
+      canvas.height
+    ) {
+      setTimeout(() => {
+        invaderProjectiles.splice(index, 1);
+      }, 0);
+    } else {
+      invaderProjectile.update();
+    }
+
+    if(
+      rectangularCollision({
+        rectangle1: invaderProjectile,
+        rectangle2: player
+      })
+    ){
+      invaderProjectiles.splice(index, 1);
+      endGame();
+    }
+  });
 }
 
