ed51c32d99382ee6214a496afd9b606651a27b41
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-06-18 16:14:30 -0400

    add player movement control, updates player speed and rotation based on ArrowLeft and ArrowRight key presses.

--- index.js
+++ index.js
@@ -364,4 +364,18 @@ function animate() {
         endGame();
     }
   });
+
+  if(keys.ArrowLeft.pressed && player.position.x >= 0) {
+    player.velocity.x = -7;
+    player.rotation = -0.15;
+  } else if(
+    keys.ArrowRight.pressed &&
+    player.position.x + player.width <= canvas.width
+  ) {
+    player.velocity.x = 7;
+    player.rotation = 0.15;
+  } else {
+    player.velocity.x = 0;
+    player.rotation = 0;
+  }
 }
