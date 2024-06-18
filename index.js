8d111a9d57cffe11c0425547e09689fc35495a92
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-06-18 17:16:20 -0400

    add event to start the game when the start button is clicked.

--- index.js
+++ index.js
@@ -411,3 +411,13 @@ function animate() {
 
   frames++;
 }
+
+document.querySelector("#startButton").addEventListener("click", () => {
+  audio.backgroundMusic.play();
+  audio.start.play();
+
+  document.querySelector("#startScreen").style.display = "none";
+  document.querySelector("#scoreContainer").style.display = "block";
+  init();
+  animate();
+});
